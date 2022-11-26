b7b7ea4af77e561b5296bf1a298f11973b6a79e4
Ubuntu <ubuntu@ip-172-31-21-55.ec2.internal>
2022-11-26 17:28:47 +0000

    hello

--- envsetup.sh
+++ envsetup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -d "env" ] 
-then
-    echo "Python virtual environment exists." 
-else
-    python3 -m venv env
-fi
+# if [ -d "env" ] 
+# then
+#     echo "Python virtual environment exists." 
+# else
+#     python3 -m venv env
+# fi
 
-source env/bin/activate
+# source env/bin/activate
 
 pip3 install -r requirements.txt
 
