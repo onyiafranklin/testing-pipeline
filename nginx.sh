09e6d9a970b8a3a9a48f7777f6a2d74bb741dd1b
Ubuntu <ubuntu@ip-172-31-21-55.ec2.internal>
2022-11-28 01:25:03 +0000

    hello

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf app.conf /etc/nginx/sites-available/testing
 chmod 710 /var/lib/jenkins/workspace/cicd_project
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/testing /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
