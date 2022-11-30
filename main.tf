fd56e62981c9e2d1d4b22b4eae9f1e9b9cb288f3
Ubuntu <ubuntu@ip-172-31-21-55.ec2.internal>
2022-11-30 15:58:20 +0000

    hello

--- main.tf
+++ main.tf
@@ -2,8 +2,10 @@ provider "aws"{
     region="us-east-1"
 }
 
-resource "aws_instance" "ec2-instance" {
-  count     = 1 
+
+
+resource "aws_instance" "AWS-instance" {
+  count     = 1
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   tags = {
