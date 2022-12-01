762b5c3386f69984eb3d1b0584e91d2611b016f9
Ubuntu <ubuntu@ip-172-31-4-133.eu-west-1.compute.internal>
2022-12-01 11:47:32 +0000

    test

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ provider "aws"{
 
 resource "aws_instance" "AWS-instance" {
   count     = 1
-  ami           = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
+  ami           = "ami-0fd8802f94ed1c969"
+  instance_type = "t3.large"
   tags = {
-    Name = "Node1"
+    Name = "Grp16-devops-final"
     }
-    key_name     = "mbo"
+    key_name     = "x21221600-FOnyia-KeyPair-NCI.pem"
   
 }
