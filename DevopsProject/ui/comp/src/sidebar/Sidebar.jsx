a3f2d4dc770ad4dde80a1a3b68f989fa1538f010
Ubuntu <ubuntu@ip-172-31-83-153.ec2.internal>
2022-12-05 15:25:44 +0000

    Test Change

--- DevopsProject/ui/comp/src/sidebar/Sidebar.jsx
+++ DevopsProject/ui/comp/src/sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
-        <h1>Notes</h1>
+        <h1>Notes-(Test Change)</h1>
         <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
