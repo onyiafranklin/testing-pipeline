ccd7b68050046f4e9c89e9ee147ec8f9d85a7f11
Ubuntu <ubuntu@ip-172-31-83-153.ec2.internal>
2022-12-05 05:41:11 +0000

    kqsoks

--- DevopsProject/ui/comp/src/sidebar/Sidebar.jsx
+++ DevopsProject/ui/comp/src/sidebar/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = ({
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>Notes</h1>
-        <button onClick={onAddNote}>AddS</button>
+        <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
         {sortedNotes.length != 0 && sortedNotes.map(({ NoteId, Title, NoteDescription, UpdatedDate }, i) => (
